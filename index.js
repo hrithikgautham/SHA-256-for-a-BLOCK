b9fb7bcbaddab0cf3c407eb007fa7fdd27ae69b1
hrithik <htg2ana@gmail.com>
2019-11-28 17:17:38 +0530

    commit commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const { getHash } = require('./hash');
-
-function resolve(targetString, noOfZeros) {
-    let result = getHash(targetString, noOfZeros);
-    return result !== null ? result : null;
-}
-
-module.exports = resolve;
