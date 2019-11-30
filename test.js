9dd87919de94536ddb5ff15d817194d37a599d5a
hrithik <htg2ana@gmail.com>
2019-11-30 10:37:33 +0530

    ignored a.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { getHash } = require('./');
-
-// 429590ad5e0c7b7fec022b536dd67bb4727f951b843eb5ea4fc34ded5b2ab8b5
-
-getHash("429590ad5e0c7b7fec022b536dd67bb4727f951b843eb5ea4fc34ded5b92a8b5", 4, ".")
-    .then(console.log)
-    .catch(err => console.error("Error in test.js, ", err));
-// console.log("sdsdfds")
